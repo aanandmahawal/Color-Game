41900396bd6a9cb9a5e0748d57c2f92a4b104bb9
aanandmahawal <151204835+aanandmahawal@users.noreply.github.com>
2025-09-20 08:11:18 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,28 @@
+
+document.body.addEventListener("click", (event)=>{
+  
+  const circle = document.createElement('div');
+  circle.className = 'circle';
+  circle.textContent = 'Hi';
+  
+  const x = event.clientX;
+  const y = event.clientY;
+
+  circle.style.left = `${x-25}px`
+  circle.style.top= `${y-25}px`
+
+
+  const color = ["violet", "indigo", "blue", "green" , "yellow" , "orange", "red"];
+  circle.style.backgroundColor = color[Math.floor(Math.random()*color.length)];
+
+  const messages = ['Rohit', 'Virat', 'Welcome', 'Hey there', 'Dhoni'];
+  circle.textContent = messages[Math.floor(Math.random() * messages.length)];
+  
+
+  document.body.appendChild(circle);
+
+  setTimeout(()=>{
+    circle.remove();
+  } ,5000)
+
+})
